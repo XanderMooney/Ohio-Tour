cc81e7f122e1c77826822c05f5c94de4866f925e
Xander Mooney <113313704+XanderMooney@users.noreply.github.com>
2023-05-03 10:21:32 -0400

    spinny bus

--- threejs/bus.js
+++ threejs/bus.js
@@ -1,17 +1,50 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
+import { Box3 } from 'three'
 
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer( { alpha: true });
-renderer.setSize(window.innerWidth /4, window.innerHeight/4, false);
-document.body.appendChild(renderer.domElement);
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000)
+const renderer = new THREE.WebGLRenderer({ alpha: true })
+renderer.setSize(window.innerWidth, window.innerHeight, false)
 renderer.domElement.classList.add('canvas')
+renderer.outputEncoding = THREE.sRGBEncoding
+document.body.appendChild(renderer.domElement)
 
-const modelLoader = new GLTFLoader();
-modelLoader.load('models/japanese_bus_nagoya_city_bus_aichi/scene.gltf'), function (gltf) {
-    scene.add(gltf.scene);
-}, undefined, function (error) {
-    console.error("your model did an oopsie")
+const loader = new GLTFLoader()
+let vector;
+loader.load('models/japanese_bus_nagoya_city_bus_aichi/scene.gltf', function(gltf) {
+  const model = gltf.scene
+  scene.add(model)
+
+  const box3 = new Box3().setFromObject(gltf.scene)
+  vector = new THREE.Vector3()
+  box3.getCenter(vector)
+  model.position.set(-vector.x, -vector.y, -vector.z)
+
+  animate()
+}, undefined, function(error) {
+  console.log(error)
+});
+
+let radius = 8;
+let time = 0;
+let clock = new THREE.Clock()
+function animate() {
+
+  time += clock.getDelta() / 10
+
+  camera.position.x = Math.sin(time) * radius
+  camera.position.z = Math.cos(time) * radius
+  camera.lookAt(0, 0, 0)
+
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
 }
+
+addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.fov = 2 * Math.atan(Math.tan(20) / (16 / 10 * camera.aspect)) * 180 / Math.PI
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight, false)
+})
