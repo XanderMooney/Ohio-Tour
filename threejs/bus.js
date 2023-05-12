fe9a2c03fd3bbb4bbf0c147f7ee7eb611f2aed0a
Xander Mooney <113313704+XanderMooney@users.noreply.github.com>
2023-05-12 10:27:56 -0400

    tons of model fixes

--- threejs/bus.js
+++ threejs/bus.js
@@ -4,6 +4,8 @@ import { Box3 } from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 const scene = new THREE.Scene()
+const light = new THREE.AmbientLight('0xffffff')
+scene.add(light);
 const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer({ alpha: true })
 renderer.setSize(window.innerWidth, window.innerHeight, false)
--- threejs/food.js
+++ threejs/food.js
@@ -4,28 +4,51 @@ import { Box3 } from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000)
+const light = new THREE.AmbientLight(0xffffff)
+scene.add(light);
 const renderer = new THREE.WebGLRenderer({ alpha: true })
+const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000)
 renderer.setSize(window.innerWidth, window.innerHeight, false)
 renderer.domElement.classList.add('canvas')
 renderer.outputEncoding = THREE.sRGBEncoding
+renderer.outputColorSpace = THREE.SRGBColorSpace
 document.body.appendChild(renderer.domElement)
 
 const loader = new GLTFLoader()
-// let vector;
-// loader.load('models/japanese_bus_nagoya_city_bus_aichi/scene.gltf', function(gltf) {
-//   const model = gltf.scene
-//   scene.add(model)
+loader.load("../models/hamburger/scene.gltf", function(gltf) {
+  const model = gltf.scene
+  let vector;
+  
+  scene.add(model)
+  
+  model.rotation.set(0.1, 2.4, 0.6);
 
-//   const box3 = new Box3().setFromObject(gltf.scene)
-//   vector = new THREE.Vector3()
-//   box3.getCenter(vector)
-//   model.position.set(-vector.x, -vector.y, -vector.z)
+  const box3 = new Box3().setFromObject(gltf.scene)
+  vector = new THREE.Vector3()
+  box3.getCenter(vector)
+  model.position.set(-vector.x + 35, -vector.y + 90, -vector.z - 120)
 
-//   animate()
-// }, undefined, function(error) {
-//   console.log(error)
-// });
+  animate()
+}, undefined, function(error) {
+  console.log(error)
+});
+loader.load("../models/taco-from-poly-by-google/taco.gltf", function(gltf) {
+  const model = gltf.scene
+  let vector;
+  
+  model.rotation.set(0, -2.3, -0.3);
+
+  scene.add(model)
+
+  const box3 = new Box3().setFromObject(gltf.scene)
+  vector = new THREE.Vector3()
+  box3.getCenter(vector)
+  model.position.set(-vector.x, -vector.y, -vector.z - 5)
+
+  animate()
+}, undefined, function(error) {
+  console.log(error)
+});
 
 let radius = 8;
 let time = 0;
@@ -47,6 +70,7 @@ addEventListener("resize", () => {
 
 // Thanks to Jack Kennedy for this lovely repsonsiveness
 function convertFov(fov, vw, vh) {
+  console.log("ran")
   const DEVELOPER_SCREEN_ASPECT_RATIO_HEIGHT = 10;
   const DEVELOPER_SCREEN_ASPECT_RATIO_WIDTH = 16;
   
